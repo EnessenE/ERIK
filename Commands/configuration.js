1a74a3f996f91ee7149f115879a227f812781c9d
Enes Keric <mail.enes.nu@gmail.com>
2018-06-09 20:27:01 +0200

    Fix inconsistent == use

--- Commands/configuration.js
+++ Commands/configuration.js
@@ -6,7 +6,7 @@ function PermCheck(message, user, roleid) {
     return new Promise(function (resolve, reject) {
         roletarget = parseInt(roleid);
         message.member.roles.forEach(function (element) {
-            if (roletarget == parseInt(element.id)) {
+            if (roletarget === parseInt(element.id)) {
                 val = true;
             }
         });
@@ -14,7 +14,7 @@ function PermCheck(message, user, roleid) {
             val = true;
         }
         resolve(val);
-    })
+    });
 }
 
 module.exports = {
@@ -24,36 +24,36 @@ module.exports = {
     },
 
     setprefix: async function (client, message, parameters) {
-        if (await PermCheck(message, message.author, gotroleid) == true) {
-            if (parameters.length != 0) {
+        if (await PermCheck(message, message.author, gotroleid) === true) {
+            if (parameters.length !== 0) {
                 //prefixset[message.guild.id] = parameters[0];
-                sql.setprefix(message.guild.id, parameters[0])
+                sql.setprefix(message.guild.id, parameters[0]);
                 message.reply("Changed the prefix from " + prefix + " to " + parameters[0] + ".");
             }
         }
         else {
-            message.reply(notallowed("prefix", message.guild.id))
+            message.reply(notallowed("prefix", message.guild.id));
         }
     },
     setbotcontrol: async function (message) {
         if (message.member.hasPermission("ADMINISTRATOR")) {
-            if (parameters[0] != ("" || undefined)) {
+            if (parameters[0] !== ("" || undefined)) {
                 bigpara = "";
                 for (var i = 0; i < parameters.length; i++) {
-                    bigpara = bigpara + " " + parameters[i]
+                    bigpara = bigpara + " " + parameters[i];
                 }
                 bigpara = bigpara.substr(1, bigpara.length);
                 var found = false;
                 var roleid = 0;
                 var rolename = "";
                 message.guild.roles.forEach(function (element) {
-                    if (element.name == bigpara) {
+                    if (element.name === bigpara) {
                         found = true;
                         roleid = element.id;
                         rolename = element.name;
                     }
                 });
-                if (found == true) {
+                if (found === true) {
                     if (await sql.updatevalue(message.guild.id, "PermRole", roleid)) {
                         message.reply("I have set the role " + rolename + " to control me.");
                     }
@@ -69,13 +69,13 @@ module.exports = {
                 roleid = 0;
                 rolename = "";
                 message.guild.roles.forEach(function (element) {
-                    if (element.id == gotroleid) {
+                    if (element.id === gotroleid) {
                         found = true;
                         roleid = element.id;
                         rolename = element.name;
                     }
                 });
-                if (roleid != 0) {
+                if (roleid !== 0) {
                     message.reply("The role that can control me is " + rolename + ".");
                 }
                 else {
@@ -87,4 +87,4 @@ module.exports = {
             message.reply("Sorry, you need the Administrator permission to change this.");
         }
     }
-}
+};
