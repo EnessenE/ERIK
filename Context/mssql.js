0dd2ff23269510800e64047cd99e2a2bafbd0d97
Enes Keric <EnessenE@users.noreply.github.com>
2018-12-09 19:53:19 +0100

    Update mssql.js
    Retrieve answer from mssql

--- Context/mssql.js
+++ Context/mssql.js
@@ -61,46 +61,39 @@ async function getServer(serverid) {
 }
 
 async function createServer(id, servername, members, prefix, owner, ownerid, region) {
-    var result = false;
-    print("Creating server")
-    print(id + servername + members + prefix + owner + ownerid + region);
-    try {
-        const request = new sql.Request(pool);
+    return new Promise(function (resolve, reject) {
+        print("Creating server for " + servername);
+        print(id + servername + members + prefix + owner + ownerid + region);
+        try {
+            const request = new sql.Request(pool);
 
-        request.input('id', sql.NVarChar(sql.MAX), id);
-        request.input('name', sql.NVarChar(sql.MAX), servername);
-        request.input('members', sql.NVarChar(sql.MAX), members);
-        request.input('prefix', sql.NVarChar(5), prefix);
-        request.input('ownerid', sql.NVarChar(sql.MAX), ownerid);
-        request.input('owner', sql.NVarChar(sql.MAX), owner);
-        request.input('region', sql.NVarChar(sql.MAX), region);
+            request.input('id', sql.NVarChar(sql.MAX), id);
+            request.input('name', sql.NVarChar(sql.MAX), servername);
+            request.input('members', sql.NVarChar(sql.MAX), members);
+            request.input('prefix', sql.NVarChar(5), prefix);
+            request.input('ownerid', sql.NVarChar(sql.MAX), ownerid);
+            request.input('owner', sql.NVarChar(sql.MAX), owner);
+            request.input('region', sql.NVarChar(sql.MAX), region);
 
-        request.execute('createServer', (err, result) => {
-            if (err) {
-                print("Error while creating record: " + err, true);
-            }
-
-            if (result != undefined) {
-                if (result.recordset != undefined) {
-                    print(result.recordsets.length) // count of recordsets returned by the procedure
-                    print(result.recordsets[0].length) // count of rows contained in first recordset
-                    print(result.recordset) // first recordset from result.recordsets
+            request.execute('createServer', (err, result) => {
+                if (err) {
+                    print("Error while creating record: " + err, true);
                 }
-                print(result.returnValue) // procedure return value
-                print(result.output) // key/value collection of output values
-                print(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
-                result = true;
-            }
-            else {
-                print("Didn't recieve a result");
-            };
-        });
-    }
-    catch (error) {
-        print("failed at getting prefix")
-        print(error);
-    }
-    return result;
+
+                if (result != undefined) {
+                    resolve(true)
+                }
+                else {
+                    print("Didn't recieve a result");
+                };
+            });
+        }
+        catch (error) {
+            print("failed at create server for " + servername);
+            print(error);
+            resolve(false);
+        }
+    });
 }
 
 async function updateServer() {
@@ -112,37 +105,34 @@ async function setPrefix() {
 }
 
 async function getPrefix(serverid) {
-    var result = null;
-    try {
-        const request = new sql.Request(pool);
-        request.input('id', sql.NVarChar(sql.MAX), serverid);
-        request.execute('getPrefix', (err, result) => {
-            if (err) {
-                print("Error while getting prefix: " + err, true);
-            }
-
-            if (result != undefined) {
-                if (result.recordset != undefined) {
-                    print(result.recordsets.length) // count of recordsets returned by the procedure
-                    print(result.recordsets[0].length) // count of rows contained in first recordset
-                    print(result.recordset) // first recordset from result.recordsets
+    return new Promise(function (resolve, reject) {
+        try {
+            const request = new sql.Request(pool);
+            request.input('id', sql.NVarChar(sql.MAX), serverid);
+            request.execute('getPrefix', (err, result) => {
+                if (err) {
+                    print("Error while getting prefix: " + err, true);
                 }
-                print(result.returnValue) // procedure return value
-                print(result.output) // key/value collection of output values
-                print(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
-                result = result.recordsets[0];
-                print("Success")
-            }
-            else {
-                print("Didn't recieve a result");
-            }
-        });
-    }
-    catch (error) {
-        print("failed at getting prefix");
-        print(error);
-    }
-    return result;
+
+                if (result != undefined) {
+                    if (result.recordset != undefined) {
+                        var answer = result.recordset[0].prefix;
+                        print("I think: " + answer);
+                        resolve(answer);
+                    }
+                }
+                else {
+                    print("Didn't recieve a result");
+                    resolve(null);
+                }
+            });
+        }
+        catch (error) {
+            print("failed at getting prefix");
+            print(error);
+            resolve(null);
+        }
+    });
 }
 
 module.exports = {
