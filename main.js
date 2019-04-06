dbd2eeffcb624a779ace0e55aafcdc982be8194d
Enes <9263360+EnessenE@users.noreply.github.com>
2019-04-06 09:35:44 +0200

    Fix a bracket

--- main.js
+++ main.js
@@ -144,252 +144,250 @@ function initialize_main() {
 }
 
 function clienterror(error) {
-    {
-        console.log('Error event:\n' + JSON.stringify(error));
-        // handle the error here
+    console.log('Error event:\n' + JSON.stringify(error));
+    // handle the error here
+}
 
+async function memberJoined(member) {
+    print(`${member.user.username} has joined ${member.guild.name}`)
+}
+
+async function memberLeft(member) {
+    print(`${member.user.username} has left ${member.guild.name}`)
+    if (repo.getLeaveMessages()) {
+        member.guild.channels.forEach(function (element) {
+            if (element.name == "general") {
+                element.send(`<@${member.user.id}> has left this server. Wave goodbye.`);
+            }
+        });
+        //TODO: Cache the userid that has left incase he rejoins so we can reassign roles.
     }
+}
 
-    async function memberJoined(member) {
-        print(`${member.user.username} has joined ${member.guild.name}`)
-    }
+function joinedGuild(guild) {
+    const embed = new RichEmbed()
+    embed.setTitle("General information")
+    embed.setColor("#00a9ff")
+    embed.setDescription("This bot has joined a new guild");
 
-    async function memberLeft(member) {
-        print(`${member.user.username} has left ${member.guild.name}`)
-        if (repo.getLeaveMessages()) {
-            member.guild.channels.forEach(function (element) {
-                if (element.name == "general") {
-                    element.send(`<@${member.user.id}> has left this server. Wave goodbye.`);
-                }
-            });
-            //TODO: Cache the userid that has left incase he rejoins so we can reassign roles.
-        }
-    }
+    embed.setThumbnail(guild.iconURL);
+    embed.addField("Name", guild.name);
+    embed.addField("Members", guild.memberCount);
+    embed.setTimestamp(new Date());
+    SendToAdmin(embed);
+}
 
-    function joinedGuild(guild) {
-        const embed = new RichEmbed()
-        embed.setTitle("General information")
-        embed.setColor("#00a9ff")
-        embed.setDescription("This bot has joined a new guild");
-
-        embed.setThumbnail(guild.iconURL);
-        embed.addField("Name", guild.name);
-        embed.addField("Members", guild.memberCount);
-        embed.setTimestamp(new Date());
-        SendToAdmin(embed);
-    }
-
-    async function messageEvent(message) {
-        try {
-            if (message.author != client.user) {
-                if (message.channel.type === 'dm') {
-                    message.reply(`Hi! I have no functioning commands here. If you want to talk about me contact${contacts}. Or to add me visit ${config.info.invitelink}`);
-                }
-                else {
-                    if (message.guild.available) {
-                        const user = message.author;
-
-                        var serverdata = await repo.GetServer(await message.guild.id);
-
-                        if (serverdata === null) {
-                            var result = await repo.CreateServer(message.guild.id, message.guild.name, message.guild.memberCount, config.default.prefix, message.guild.owner.user.tag, message.guild.ownerID, message.guild.region);
-                            print("Creation of record: " + result, true);
-                        }
-                        else {
-                            print("Exists, have to update")
-                            //TODO: apply cache-ing here
-                            //repo.UpdateServer(message.guild.id, message.guild.name, message.guild.memberCount, await message.guild.ownerID, message.guild.region);
-                        }
-
-                        if (user.tag !== client.user.tag) {
-                            print(`[${message.guild.name}]${message.author.tag} - ${message.content}`);
-
-                            var messageParts = message.content.split(' ');
-                            var input = messageParts[0].toLowerCase();
-
-                            var parameters = messageParts.splice(1, messageParts.length);
-
-                            var prefix = await getPrefix(message.guild.id);
-                            //var role_id = await repo.GetValue(message.guild.id, "PermRole");
-
-                            try {
-                                if (prefix != null) {
-                                    GuildSpecificChecks(message);
-                                    if (input.charAt(0) == prefix) {
-                                        var command = input.substr(1);
-                                        commandLogic(prefix, null, message, command, parameters);
-                                    }
-                                }
-                            }
-                            catch (error) {
-                                print("Error: " + error);
-                                sendErrorToAdmin("Error occured in commandLogic", error, message);
-                            }
-
-                        }
-                    }
-                }
-            }
-        }
-        catch (error) {
-            sendErrorToAdmin("Error occured in messageEvent", error, message);
-        }
-    }
-
-    async function commandLogic(prefix, role_id, message, command, parameters) {
-        try {
-            if (command === "ping") {
-                infocommands.ping(client, message);
-            }
-            else if (command === "you" || command === "botinfo") {
-                infocommands.botinfo(client, message);
-            }
-            else if (command === "prefix") {
-                print(`${message.author.tag} has this permission: ${await hasPermission(message)}`);
-                if (await hasPermission(message)) {
-                    configcommands.setprefix(client, prefix, message, parameters);
-                }
-                else {
-                    message.reply(notallowed());
-                }
-            }
-            else if (command === "serverinfo") {
-                infocommands.serverinfo(client, message);
-            }
-            else if (command === "help" || command === "commands") {
-                infocommands.help(client, prefix, message, commands);
-            }
-            else if (command === "botcontrol") {
-                if (await hasPermission(message)) {
-                    configcommands.setbotcontrol(message, parameters);
-                }
-                else {
-                    message.reply(notallowed());
-                }
-            }
-            else if ((command === "userinfo") || (command === "me")) {
-                infocommands.userinfo(client, message, parameters);
+async function messageEvent(message) {
+    try {
+        if (message.author != client.user) {
+            if (message.channel.type === 'dm') {
+                message.reply(`Hi! I have no functioning commands here. If you want to talk about me contact${contacts}. Or to add me visit ${config.info.invitelink}`);
             }
             else {
-                GuildSpecificCommands(message);
+                if (message.guild.available) {
+                    const user = message.author;
+
+                    var serverdata = await repo.GetServer(await message.guild.id);
+
+                    if (serverdata === null) {
+                        var result = await repo.CreateServer(message.guild.id, message.guild.name, message.guild.memberCount, config.default.prefix, message.guild.owner.user.tag, message.guild.ownerID, message.guild.region);
+                        print("Creation of record: " + result, true);
+                    }
+                    else {
+                        print("Exists, have to update")
+                        //TODO: apply cache-ing here
+                        //repo.UpdateServer(message.guild.id, message.guild.name, message.guild.memberCount, await message.guild.ownerID, message.guild.region);
+                    }
+
+                    if (user.tag !== client.user.tag) {
+                        print(`[${message.guild.name}]${message.author.tag} - ${message.content}`);
+
+                        var messageParts = message.content.split(' ');
+                        var input = messageParts[0].toLowerCase();
+
+                        var parameters = messageParts.splice(1, messageParts.length);
+
+                        var prefix = await getPrefix(message.guild.id);
+                        //var role_id = await repo.GetValue(message.guild.id, "PermRole");
+
+                        try {
+                            if (prefix != null) {
+                                GuildSpecificChecks(message);
+                                if (input.charAt(0) == prefix) {
+                                    var command = input.substr(1);
+                                    commandLogic(prefix, null, message, command, parameters);
+                                }
+                            }
+                        }
+                        catch (error) {
+                            print("Error: " + error);
+                            sendErrorToAdmin("Error occured in commandLogic", error, message);
+                        }
+
+                    }
+                }
             }
         }
-        catch (error) {
-            throw error;
-        }
     }
+    catch (error) {
+        sendErrorToAdmin("Error occured in messageEvent", error, message);
+    }
+}
 
-    function GuildSpecificCommands(message) {
-        print("GuildSpecificCommands");
-        var pass = false;
-        for (var i = 0; i < config.settings.specialguilds.length; i++) {
-            if (message.guild.id == config.settings.specialguilds[i]) {
-                pass = true;
-
-
-                message.attachments.forEach(function (element) {
-                    print("checking file: " + element.filename);
-                    for (var i = 0; i < config.settings.blockedfilenames.length; i++) {
-                        if (element.filename == config.settings.blockedfilenames[i]) {
-                            print("BLOCKED FILE");
-                            message.reply("Bad boy!");
-                            message.delete();
-                        }
-                    }
-                });
-
+async function commandLogic(prefix, role_id, message, command, parameters) {
+    try {
+        if (command === "ping") {
+            infocommands.ping(client, message);
+        }
+        else if (command === "you" || command === "botinfo") {
+            infocommands.botinfo(client, message);
+        }
+        else if (command === "prefix") {
+            print(`${message.author.tag} has this permission: ${await hasPermission(message)}`);
+            if (await hasPermission(message)) {
+                configcommands.setprefix(client, prefix, message, parameters);
+            }
+            else {
+                message.reply(notallowed());
             }
         }
+        else if (command === "serverinfo") {
+            infocommands.serverinfo(client, message);
+        }
+        else if (command === "help" || command === "commands") {
+            infocommands.help(client, prefix, message, commands);
+        }
+        else if (command === "botcontrol") {
+            if (await hasPermission(message)) {
+                configcommands.setbotcontrol(message, parameters);
+            }
+            else {
+                message.reply(notallowed());
+            }
+        }
+        else if ((command === "userinfo") || (command === "me")) {
+            infocommands.userinfo(client, message, parameters);
+        }
+        else {
+            GuildSpecificCommands(message);
+        }
     }
+    catch (error) {
+        throw error;
+    }
+}
+
+function GuildSpecificCommands(message) {
+    print("GuildSpecificCommands");
+    var pass = false;
+    for (var i = 0; i < config.settings.specialguilds.length; i++) {
+        if (message.guild.id == config.settings.specialguilds[i]) {
+            pass = true;
 
 
-    async function GuildSpecificChecks(message) {
-        print("GuildSpecificCommands");
-        var pass = false;
-        for (var i = 0; i < config.settings.specialguilds.length; i++) {
-            if (message.guild.id == config.settings.specialguilds[i]) {
-                pass = true;
-                message.attachments.forEach(function (element) {
-                    print("checking file: " + element.filename);
-                    for (var i = 0; i < config.settings.blockedfilenames.length; i++) {
-                        if (element.filename == config.settings.blockedfilenames[i]) {
-                            print("BLOCKED FILE");
-                            message.reply("Bad boy!");
-                            message.delete();
-                        }
-                    }
-                    for (var i = 0; i < config.settings.blockedfilesize.length; i++) {
-                        print("SIZE: " + element.filesize);
-                        if (element.filesize == config.settings.blockedfilesize[i]) {
-                            print("BLOCKED FILE");
-                            message.reply("Bad boy!");
-                            message.delete();
-                        }
-                    }
-                });
-                for (var i = 0; i < config.settings.blockedwords.length; i++) {
-                    if (message.content.includes(config.settings.blockedwords[i])) {
-                        print("BLOCKED WORD");
+            message.attachments.forEach(function (element) {
+                print("checking file: " + element.filename);
+                for (var i = 0; i < config.settings.blockedfilenames.length; i++) {
+                    if (element.filename == config.settings.blockedfilenames[i]) {
+                        print("BLOCKED FILE");
                         message.reply("Bad boy!");
                         message.delete();
                     }
                 }
+            });
 
-            }
         }
     }
+}
 
-    function notallowed() {
-        return "you are not allowed to use that command.";
-    }
 
-    async function hasPermission(message) {
-        var val = false;
-        return new Promise(async function (resolve, reject) {
-            try {
-                roletarget = parseInt(await repo.GetControl(message.guild.id));
-                message.member.roles.forEach(function (element) {
-                    if (roletarget == parseInt(element.id)) {
-                        val = true;
+async function GuildSpecificChecks(message) {
+    print("GuildSpecificCommands");
+    var pass = false;
+    for (var i = 0; i < config.settings.specialguilds.length; i++) {
+        if (message.guild.id == config.settings.specialguilds[i]) {
+            pass = true;
+            message.attachments.forEach(function (element) {
+                print("checking file: " + element.filename);
+                for (var i = 0; i < config.settings.blockedfilenames.length; i++) {
+                    if (element.filename == config.settings.blockedfilenames[i]) {
+                        print("BLOCKED FILE");
+                        message.reply("Bad boy!");
+                        message.delete();
                     }
-                });
-                if (message.member.hasPermission("ADMINISTRATOR")) {
-                    val = true;
                 }
-                resolve(val);
-            }
-            catch (error) {
-                resolve(false);
-            }
-        });
-    }
-
-
-    function IsAdmin(id) {
-        return new Promise(function (resolve, reject) {
-            config.settings.admins.forEach(async function (x) {
-                if (x == id) {
-                    resolve(true);
+                for (var i = 0; i < config.settings.blockedfilesize.length; i++) {
+                    print("SIZE: " + element.filesize);
+                    if (element.filesize == config.settings.blockedfilesize[i]) {
+                        print("BLOCKED FILE");
+                        message.reply("Bad boy!");
+                        message.delete();
+                    }
                 }
             });
+            for (var i = 0; i < config.settings.blockedwords.length; i++) {
+                if (message.content.includes(config.settings.blockedwords[i])) {
+                    print("BLOCKED WORD");
+                    message.reply("Bad boy!");
+                    message.delete();
+                }
+            }
+
+        }
+    }
+}
+
+function notallowed() {
+    return "you are not allowed to use that command.";
+}
+
+async function hasPermission(message) {
+    var val = false;
+    return new Promise(async function (resolve, reject) {
+        try {
+            roletarget = parseInt(await repo.GetControl(message.guild.id));
+            message.member.roles.forEach(function (element) {
+                if (roletarget == parseInt(element.id)) {
+                    val = true;
+                }
+            });
+            if (message.member.hasPermission("ADMINISTRATOR")) {
+                val = true;
+            }
+            resolve(val);
+        }
+        catch (error) {
             resolve(false);
+        }
+    });
+}
+
+
+function IsAdmin(id) {
+    return new Promise(function (resolve, reject) {
+        config.settings.admins.forEach(async function (x) {
+            if (x == id) {
+                resolve(true);
+            }
         });
-    }
+        resolve(false);
+    });
+}
 
-    async function Start_Bot() {
-        configcommands.init(repo, config);
-        infocommands.init(repo, config, OS);
-        time.init(repo, config);
+async function Start_Bot() {
+    configcommands.init(repo, config);
+    infocommands.init(repo, config, OS);
+    time.init(repo, config);
 
-        await initialize_misc();
-        await client.login(config.token.discord);
+    await initialize_misc();
+    await client.login(config.token.discord);
 
-        print("Bot has started", true);
-        initialize_main();
+    print("Bot has started", true);
+    initialize_main();
 
-    }
+}
 
-    Start_Bot();
+Start_Bot();
 
 
 
