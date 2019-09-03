f328825e36e3a7bca835bb62c800fb7ee730749a
Enes Keric <e.keric@student.fontys.nl>
2019-09-03 13:47:52 +0200

    Add logger

---
+++ ERIK/Helpers/Logger.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERIK.Helpers
+{
+    static class Logger
+    {
+        internal static Task Log(LogMessage msg)
+        {
+            return Log(msg.ToString());
+        }
+
+        internal static Task Log(String msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
