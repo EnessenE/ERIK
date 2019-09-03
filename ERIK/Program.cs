fad4b609224b9aeabd694268bf3d069954432b81
Enes Keric <e.keric@student.fontys.nl>
2019-09-03 14:42:27 +0200

    Add basic code to connect to discord

--- ERIK/Program.cs
+++ ERIK/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Discord;
+using Discord.WebSocket;
+using ERIK.Helpers;
+using System;
 using System.Threading.Tasks;
 
 namespace ERIK
 {
     class Program
     {
-        static void Main(string[] args)
+        protected DiscordSocketClient _client;
+
+        internal static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        internal async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Logger.Log;
+
+            string DiscordToken = Properties.Settings.Default.DiscordToken;
+
+            await Logger.Log("Started client with token  " + DiscordToken);
+
+            await _client.LoginAsync(TokenType.Bot, DiscordToken);
+            await _client.StartAsync();
+
+            //Events
+            _client.MessageReceived += MessageReceived;
+
+            await Task.Delay(-1);
         }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+
     }
 }
